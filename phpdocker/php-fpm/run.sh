107444eca8b8b5b44e4144ab3164c27599318b6d
Karheda <carlos@nectarestudio.com>
2019-12-30 13:12:11 +0100

    Mejoras en proxy inverso

--- phpdocker/php-fpm/run.sh
+++ phpdocker/php-fpm/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Running composer.phar..."
-composer.phar install
-php app/console assets:install
-php app/console cache:clear
+# echo "Running composer.phar..."
+# composer.phar install
+# php app/console assets:install
+# php app/console cache:clear
 php-fpm
